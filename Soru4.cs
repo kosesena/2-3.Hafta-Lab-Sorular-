3aad6b0653ac65cb8b281c4489a6fd5dc6908cb5
Sena Köse <147408306+kosesena@users.noreply.github.com>
2024-10-29 22:15:54 +0300

    Add files via upload

---
+++ Soru4.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Soru4
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.Write("Bir Matematiksel ifade girin: ");
+                string ifade = Console.ReadLine();
+                //DataTable kullanarak ifadeyi hesapla
+                DataTable dt = new DataTable();
+                var sonuc = dt.Compute(ifade, "");
+
+                Console.WriteLine($"\nİfadenin sonucu: {sonuc}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Bir hata oluştu: " + ex.Message);
+            }
+
+            // Programın kapanmaması için bekleme
+            Console.WriteLine("Çıkmak için bir tuşa basın...");
+            Console.ReadKey();
+        }
+    }
+}
