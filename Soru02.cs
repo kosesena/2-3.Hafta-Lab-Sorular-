f13322d996ea1dc9b0763bd40905d425435f1a28
Sena Köse <147408306+kosesena@users.noreply.github.com>
2024-12-09 03:53:14 +0300

    Update Soru02.cs

--- Soru02.cs
+++ Soru02.cs
@@ -4,20 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Soru2
+namespace Soru2 // namespace Question2
 {
     class Program
     {
         static void Main()
         {
-            List<int> sayilar = new List<int>();
-            int sayi;
+            List<int> sayilar = new List<int>();    // List<int> numbers = new List<int>()
+            
+            int sayi;                            // int number
 
-            //Kullanıcıdan pozitif tam sayı alma işlemi
+           
+            // Process of taking a positive integer from the user
+            // Kullanıcıdan pozitif tam sayı alma işlemi
             do
 
             {
-                Console.Write("Bir pozitif tam sayı girin(0 yazarak tam sayı alma işlemini bitir): ");
+                Console.Write("Bir pozitif tam sayı girin(0 yazarak tam sayı alma işlemini bitir): "); // ("Enter a positive integer(Enter 0 to end the integer input process): ")
                 sayi = int.Parse(Console.ReadLine());
 
                 if (sayi > 0)
@@ -26,29 +29,30 @@ namespace Soru2
                 }
                 else if (sayi != 0)
                 {
-                    Console.WriteLine("Lütfen geçerli bir tam sayı girin. ");
+                    Console.WriteLine("Lütfen geçerli bir tam sayı girin. "); // ("Please enter a valid integer. ")
                 }
 
-            } while (sayi != 0);   // Kullanıcı 0 girene kadar devam eder
+            } while (sayi != 0);   // Kullanıcı 0 girene kadar devam eder - Continues until the user enters 0
 
-            //Kullanıcı 0 girdikten sonra ortalama ve medyanı hesaplama
+            // Kullanıcı 0 girdikten sonra ortalama ve medyanı hesaplama - Calculate the average and median after the user enters 0
 
             if (sayilar.Count > 0)
             {
            
-            //Ortalama hesaplama
-                double ortalama = sayilar.Average();
+            // Ortalama hesaplama - Calculating the average
+                double ortalama = sayilar.Average(); // double average
                 Console.WriteLine("\nOrtalama: " + ortalama);
 
-            //Medyan hesaplama
+            // Medyan hesaplama - Calculating the median
                 sayilar.Sort();
-                double medyan;
+                double medyan; // double median
                 int ortaIndex = sayilar.Count / 2;
+               
                 if (sayilar.Count % 2 == 0) //Çift sayı varsa ortadaki iki sayının ortalaması
                 {
                     medyan = (sayilar[ortaIndex - 1] + sayilar[ortaIndex]) / 2.0;
                 }
-                else //Tek sayı varsa,ortadaki sayı medyan
+                else // Tek sayı varsa,ortadaki sayı medyan - If there is an odd number of elements,the middle number is the median
                 {
                     medyan = sayilar[ortaIndex];
                 }
@@ -58,6 +62,8 @@ namespace Soru2
             {
                 Console.WriteLine("Geçerli bir sayı girilmedi. ");
             }
+            
+           
             // Sonuçları görmek için programın kapanmasını engelleme
             Console.WriteLine("Çıkmak için bir tuşa basın... ");
             Console.ReadKey();
